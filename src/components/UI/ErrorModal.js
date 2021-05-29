ac00d203f3c9663f2a06fc07b1d5624ae6cc0829
eddypastika <pastikaeddy@gmail.com>
2021-05-29 22:45:16 +0700

    add react portals, render modal outside the main App

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,29 +1,45 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import classes from "./ErrorModal.module.css"
 import Card from "./Card";
 import Button from "./Button";
 
-const ErrorModal = props => {
-    const clearErrorHandler = () => {
-        props.onClearError();
-    };
+const Backdrop = props => {
+    return <div onClick={props.onClearError} className={classes.backdrop}/>;
+};
 
+const ModalOverlay = props => {
     return (
-        <div>
-            <div onClick={clearErrorHandler} className={classes.backdrop}/>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={clearErrorHandler}>Close</Button>
-                </footer>
-            </Card>
-        </div>
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onClearError}>Close</Button>
+            </footer>
+        </Card>
+    );
+};
+
+const ErrorModal = props => {
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop onClearError={props.onClearError}/>,
+                document.getElementById("backdrop-root")
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onClearError={props.onClearError}/>,
+                document.getElementById("overlay-root")
+            )}
+        </>
     );
 };
 
